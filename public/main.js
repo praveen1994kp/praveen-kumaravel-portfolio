9c484b651f10ffe0622f27f8f56532b5e86a5078
Praveen Kumaravel <praveen1994kp@gmail.com>
2020-10-17 21:45:19 +0530

    toggle accent color, hover actions for email

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+const colorPalette = ['accent__green', 'accent__pink', 'accent__cyan', 'accent__orange'];
+
 function themeToggleListeners() {
     const themeToggle = document.getElementById('theme-toggle');
     themeToggle.addEventListener('click', function (ev) {
@@ -9,9 +11,28 @@ function themeToggleListeners() {
             toggleIcon.classList.add('fa-moon');
         } else {
             toggleIcon.classList.remove('fa-moon');
-            toggleIcon.classList.add('fa-sun');            
+            toggleIcon.classList.add('fa-sun');
         }
     })
 }
 
+function findNextAccent(currAccent) {
+    const nextAccentIndex = (colorPalette.findIndex(accent => accent == currAccent) + 1)
+        % colorPalette.length;
+
+    return colorPalette[nextAccentIndex];
+}
+
+function colorPaletteListeners() {
+    const accentToggle = document.getElementById('accent-toggle');
+    accentToggle.addEventListener('click', function (ev) {
+        const { classList } = document.body;
+        const _classList = [...classList];
+        const currAccent = _classList.find(cl => cl.startsWith('accent__'));
+        classList.remove(currAccent);
+        const nextAccent = findNextAccent(currAccent);
+        classList.add(nextAccent);
+    })
+}
 themeToggleListeners();
+colorPaletteListeners();
